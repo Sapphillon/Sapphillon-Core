ca9864c908f4500b87fb8b04b92f581b2bacb521
Yuta Takahashi <y.highbridge04@gmail.com>
2025-12-26 23:32:53 +0900

    docs: Fix typo in permission module comment

--- ext_plugin/src/permissions.rs
+++ ext_plugin/src/permissions.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Permission Related Opration
+//! Permission Related Operation
 
 use anyhow::Result;
 use deno_permissions::{Permissions, PermissionsOptions};
